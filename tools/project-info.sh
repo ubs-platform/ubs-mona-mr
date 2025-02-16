a4aaa3d35135aca54f2f51a2d4ce4931820ea85a
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-02-16 19:51:22 +0300

    project info

---
+++ tools/project-info.sh
@@ -0,0 +1,10 @@
+export VERSION=$(cat package.json | jq -r .version)
+export VERSION_TAG=$(cat package.json | jq -r .iksir | jq -r .childrenVersionTag)
+export DOCKER_ORGNAME=$(cat package.json | jq -r .docker.organisation)
+export IMG_PREFIX=$(cat package.json | jq -r .docker.imageNamePrefix)
+if [ "$VERSION_TAG" = "null" ] || [ "$VERSION_TAG" = "stable" ] || [ "$VERSION_TAG" = "latest" ]; then
+    export VERSION_TAG=''
+else
+    export VERSION_TAG="-${VERSION_TAG}"
+fi
+export DOCKER_FULL_TAG=$DOCKER_ORGNAME/$IMG_PREFIX$APP_NAME:$VERSION$VERSION_TAG
--- tools/release-app.sh
+++ tools/release-app.sh
@@ -1,25 +1,15 @@
 APP_NAME=$1
-MODE=$2
 
 if [ "$APP_NAME" = "" ]; then
     echo "APP_NAME (first argument) is required"
 else
     echo $APP_NAME
-    VERSION=$(cat package.json | jq -r .version)
-    VERSION_TAG=$(cat package.json | jq -r .iksir | jq -r .childrenVersionTag)
-    DOCKER_ORGNAME=$(cat package.json | jq -r .docker.organisation)
-    IMG_PREFIX=$(cat package.json | jq -r .docker.imageNamePrefix)
-    if [ "$VERSION_TAG" = "null" ] || [ "$VERSION_TAG" = "stable" ] || [ "$VERSION_TAG" = "latest" ]; then
-        VERSION_TAG=''
-    else
-        VERSION_TAG="-${VERSION_TAG}"
-    fi
-    DOCKER_FULL_TAG=$DOCKER_ORGNAME/$IMG_PREFIX$APP_NAME:$VERSION$VERSION_TAG
+    source $(dirname ${BASH_SOURCE[0]})/project-info.sh
     echo determined docker tag: $DOCKER_FULL_TAG
     echo "start to build $DOCKER_FULL_TAG"
     set -x
     docker build --build-arg APP_NAME="${APP_NAME}" --tag "$DOCKER_FULL_TAG" .
     set +x
-    # echo "start to push $DOCKER_FULL_TAG"
-    # docker push "$DOCKER_FULL_TAG"
+    echo "start to push $DOCKER_FULL_TAG"
+    docker push "$DOCKER_FULL_TAG"
 fi
