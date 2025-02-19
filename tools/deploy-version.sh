43d9ebc8d8dbecaef8c17ebb2896ec6514b71fb1
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-02-19 22:18:47 +0300

    Version upgrade to 5.0.666_test

--- tools/deploy-version.sh
+++ tools/deploy-version.sh
@@ -1,13 +1,15 @@
 VERSION=$1
 VERSION_TAG=$2
+
 if [ "${VERSION}" != "" ]; then
     # echo "tamam"
+    VERSION_BRANCH="v$VERSION"
     echo $(jq --arg VERSION "$VERSION" '.version=$VERSION' package.json) >package.json
     git add .
     git commit -m "Version upgrade to $VERSION"
     git push
-    git switch -c "v$VERSION"
-    git push --set-origin origin
+    git switch -c $VERSION_BRANCH
+    git push --set-upstream origin $VERSION_BRANCH
 
 else
     echo "Version is needed"
