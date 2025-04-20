57c1d180d35120301fdbd35de5145b5b8fd277a2
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-04-20 17:42:40 +0300

    Version upgrade to 5.5.0

--- tools/project-info.sh
+++ tools/project-info.sh
@@ -2,7 +2,7 @@ export VERSION=$(cat package.json | jq -r .version)
 export VERSION_TAG=$(cat package.json | jq -r .iksir | jq -r .childrenVersionTag)
 export DOCKER_ORGNAME=$(cat package.json | jq -r .docker.organisation)
 export IMG_PREFIX=$(cat package.json | jq -r .docker.imageNamePrefix)
-if [ "$VERSION_TAG" = "null" ] || [ "$VERSION_TAG" = "stable" ] || [ "$VERSION_TAG" = "latest" ]; then
+if [ "$VERSION_TAG" = "null" ] || "$VERSION_TAG" = "" ] || [ "$VERSION_TAG" = "stable" ] || [ "$VERSION_TAG" = "latest" ]; then
     export VERSION_TAG=''
 else
     export VERSION_TAG="-${VERSION_TAG}"
