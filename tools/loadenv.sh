c580805ed036743866a2cb5876ca742160c71221
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-04-21 02:34:07 +0300

    loadenv work

--- tools/loadenv.sh
+++ tools/loadenv.sh
@@ -1,6 +1,20 @@
 for var in "$@"; do
-    if [ -f $var ]; then
-        export $(cat $var | grep -v '^#' | xargs)
+    if [ -f "$var" ]; then
+        while IFS= read -r line || [ -n "$line" ]; do
+            case "$line" in
+                ''|'#'*)
+                    continue
+                    ;;
+            esac
+
+            case "$line" in
+                *=*)
+                    key=${line%%=*}
+                    value=${line#*=}
+                    export "$key=$value"
+                    ;;
+            esac
+        done < "$var"
     else
         echo "File $var does not exist. Skipping"
     fi
