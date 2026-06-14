bd349882527457c34ab9269eff6b7cd76b3f969f
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-06-14 19:27:04 +0300

    controller roles

--- libs/ip-blocker-webservice/src/ip-blocker.controller.ts
+++ libs/ip-blocker-webservice/src/ip-blocker.controller.ts
@@ -1,5 +1,8 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Logger, Post, UseGuards } from '@nestjs/common';
 import { IpBlockerService } from './ip-blocker.service';
+import { EventPattern } from '@nestjs/microservices';
+import { JwtAuthGuard } from '@ubs-platform/users-microservice-helper';
+import { Roles, RolesGuard } from '@ubs-platform/users-roles';
 
 interface ManualBanBody {
   ipAddress?: string;
@@ -12,7 +15,8 @@ interface ManualUnbanBody {
 
 @Controller('ip-blocker')
 export class IpBlockerController {
-  constructor(private readonly ipBlockerService: IpBlockerService) {}
+  private readonly logger = new Logger(IpBlockerController.name);
+  constructor(private readonly ipBlockerService: IpBlockerService) { }
 
   @Get()
   async getHello(): Promise<string> {
@@ -25,11 +29,16 @@ export class IpBlockerController {
   }
 
   @Get('bans')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(["ADMIN"])
   async getBans() {
     return this.ipBlockerService.getActiveBans();
   }
 
+
   @Post('ban')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(["ADMIN"])
   async ban(@Body() body: ManualBanBody) {
     const ipAddress = body?.ipAddress?.trim();
     if (!ipAddress) {
@@ -40,6 +49,8 @@ export class IpBlockerController {
   }
 
   @Post('unban')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(["ADMIN"])
   async unban(@Body() body: ManualUnbanBody) {
     const ipAddress = body?.ipAddress?.trim();
     if (!ipAddress) {
@@ -47,5 +58,32 @@ export class IpBlockerController {
     }
 
     return this.ipBlockerService.unbanManually(ipAddress);
+  }
+
+  @EventPattern('unban')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(["ADMIN"])
+  async handleUnbanEvent(data: { ipAddress: string }) {
+    const ipAddress = data?.ipAddress?.trim();
+    if (!ipAddress) {
+      this.logger.warn('Received unban event with missing ipAddress');
+      return;
+    }
+
+    await this.ipBlockerService.unbanManually(ipAddress);
+  }
+
+
+  @EventPattern('ban')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(["ADMIN"])
+  async handleBanEvent(data: { ipAddress: string; reason?: string }) {
+    const ipAddress = data?.ipAddress?.trim();
+    if (!ipAddress) {
+      this.logger.warn('Received ban event with missing ipAddress');
+      return;
+    }
+
+    await this.ipBlockerService.banManually(ipAddress, data.reason);
   }
 }
